21a8e04cb2490d72687085855c2077e4c4508e04
nivalen <nivalen@debian>
2017-06-30 19:50:07 +0300

    update

--- app.js
+++ app.js
@@ -9,15 +9,32 @@ const connectionURL = "mongodb://nivalen:123ndb123@ds141232.mlab.com:41232/nival
 
 let db;
 MongoClient.connect(connectionURL, function (err, database) {
-        if (err) {
-            console.error(err);
-        }
-        db = database;
-    });
+    if (err) {
+        console.error(err);
+    }
+    db = database;
+});
+
+app.use("/public", express.static("public"));
 
 app.get("/", (request, response) => {
-    db.collection("year2016").find({"school": "СУ"});
+    let arr;
+    const promise = new Promise((resolve, reject) => {
+        db.collection("year2016").find().toArray((err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                resolve(result);
+            }
+        });
+    });
+    promise.then((x) => {
+        arr = x;
+        console.log(arr);
+    });
     response.sendFile(__dirname + "/public/index.html");
 });
+
 
 app.listen(3000, () => console.log("Listening on port 3000"));
